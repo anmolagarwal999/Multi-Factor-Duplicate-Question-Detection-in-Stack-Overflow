55f191a72952b172b49c563b35ed756e43cca2c7
Cheif-Blackhood <ppratap2001@gmail.com>
2021-11-20 16:26:17 +0530

    Code and User json

---
+++ extractor.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+import datetime
+
+import config
+
+## <row Id="777711" PostTypeId="1" AcceptedAnswerId="777721" CreationDate="2009-04-22T15:07:54.137" Score="0" 
+# ViewCount="175" 
+# Body="&lt;blockquote&gt;&#xA;  &lt;p&gt;Duplicate:&#xA;  &lt;a href=&quot;http://stackoverflow.com/questions/163434/are-nulls-in-a-relational-database-okay&quot;&gt;http://stackoverflow.com/questions/163434/are-nulls-in-a-relational-database-okay&lt;/a&gt;&lt;/p&gt;&#xA;&lt;/blockquote&gt;&#xA;&#xA;&lt;p&gt;
+# I dodged a heated debate concerning nulls in the database today.&#xA;My opinion is that null is an excellent indicator of unspecified values. Every one else in the team, that has an opinion, thinks zero and empty strings are the way to go.&lt;/p&gt;&#xA;&#xA;&lt;p&gt;Are they lazy or am I to strict?&lt;/p&gt;&#xA;" 
+# OwnerUserId="21761" LastEditorUserId="44389" LastEditorDisplayName="" LastEditDate="2009-04-22T15:10:22.710" LastActivityDate="2009-04-22T15:11:00.667" 
+# ClosedDate="2009-04-22T15:11:06.530" Title="Is null harmful? [Duplicate]" Tags="&lt;asp.net&gt;&lt;sql&gt;&lt;database&gt;&lt;null&gt;" AnswerCount="4" CommentCount="1" />
+
+import xml.sax.handler
+import json
+import sys
+
+class Indexer(xml.sax.handler.ContentHandler):
+    def __init__(self, data_path="", index_path=""):
+        self.parser = xml.sax.make_parser()
+        self.parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+        self.parser.setContentHandler(self)
+        self.attributes = {}
+        self.file_num = 0
+        self.path = data_path
+        self.index_path = index_path
+        self.index = {}
+        self.index_size = 0
+        self.attribute_name = ['AcceptedAnswerId', 'AnswerCount', 'Body', 'CommentCount',
+                'CreationDate', 'FavoriteCount', 'Id', 'LastActivityDate', 'LastEditDate', 
+                'LastEditorDisplayName', 'LastEditorUserId', 'OwnerUserId', 'PostTypeId', 'Score',
+                'Tags', 'Title', 'ViewCount', 'PostId', 'RelatedPostId', 'LinkTypeId', 'TagName',
+                'Count', 'ExcerptPostId', 'WikiPostId', 'Reputation', 'DisplayName', 'EmailHash', 'LastAccessDate',
+                'AboutMe', 'rel', 'Views', 'UpVotes', 'DownVotes']
+
+    def startElement(self, name, attrs):
+        if name == 'row':
+            for name in self.attribute_name:
+                try:
+                    attr = attrs.getValue(name)
+                    self.attributes[name] = attr
+                except KeyError:
+                    pass
+
+                
+
+    def endElement(self, name):
+        if name == 'row':
+            self.index_content()
+            self.attributes = {}
+
+    def characters(self, content):
+        pass
+
+    def parse_data(self):
+        self.parser.parse(self.path)
+        self.dump_remaining()
+
+    def dump_remaining(self):
+        if self.index_size > 0:
+            self.dump_file()
+
+    def dump_file(self):
+        with open(f'{self.index_path}/post_{self.file_num}.json', 'w') as f:
+            json.dump(self.index, f)
+    
+    def init_file(self):
+        self.file_num += 1
+        self.index = {}
+        self.index_size = 0
+
+    def index_content(self):
+        
+        self.index[self.attributes['Id']] = self.attributes
+        for key, value in self.attributes.items():
+            try:
+                self.index_size += len(key) + len(value)
+            except:
+                pass
+
+        if self.index_size >= config.FILE_SIZE:
+            self.dump_file()
+            self.init_file()
+
+if __name__ == "__main__":
+    begin = datetime.datetime.now()
+    indexer = Indexer(data_path=sys.argv[1], index_path=sys.argv[2])
+    indexer.parse_data()
+    print(datetime.datetime.now() - begin)
--- preprocessing.py
+++ preprocessing.py
@@ -1,46 +1,54 @@
 import json
-from pprint import pprint
-from nltk.stem import PorterStemmer
+from nltk.stem import PorterStemmer, WordNetLemmatizer
 from collections import Counter
 from scipy import spatial
-# from nltk.tokenize import word_tokenize
 import re
-
 
 from stopwords import STOPWORDS
 
 
-class Preprocess():
-    def __init__(self, output_path) -> None:    
+class Preprocess:
+    def __init__(self, input_path, remove_small_word=False, do_lemmatization=False, ) -> None:
         self.ps = PorterStemmer()
-        self.DATADIR = output_path
+        self.lemmatizer = WordNetLemmatizer()
+        self.DATADIR = input_path
         with open(f'{self.DATADIR}/0.json', 'r') as f:
             self.questions = json.load(f)
-        pprint(len(self.questions))
-        pprint(self.questions['4'])
         self.cleaned_question = {}
         self.punc_string = "!|\(|\)|-|\[|\]|\{|\}|;|:|'|\"|\|,|<|>|/|\?|@|=|\$|%|^|&|\*|\+|_|~"
         self.punc_regex = re.compile(self.punc_string)
+        self.do_lemmatization = do_lemmatization
+        self.remove_small_word = remove_small_word
 
-    
     def remove_stopwords(self, text):
+        """remove stopwords and words have less than 3 lengths"""
+
         reduced_words = [word for word in text if word not in STOPWORDS]
+        if self.remove_small_word:
+            reduced_words = [word for word in reduced_words if len(word) > 3]
         return reduced_words
 
     def stemming(self, text: list) -> list:
         stemmed_words = [self.ps.stem(word) for word in text]
         return stemmed_words
 
+    def lemmatizing(self, text: list, pos: str = 'n') -> list:
+        lemmatized_words = [self.lemmatizer.lemmatize(word, pos=pos) for word in text]  # pos='v' mean verb
+        return lemmatized_words
+
     def parse_string(self, text):
+        """text cleaning and preprocessing"""
         text = text.lower()
         # tokenized_text = word_tokenize(text)
         text = self.punc_regex.sub(' ', text)
-        # text = re.sub(self.punc_string,'', text)
         tokenized_text = text.split()
+        if self.do_lemmatization:
+            tokenized_text = self.lemmatizing(tokenized_text, pos='v')
+        # else:
         tokenized_text = self.stemming(tokenized_text)
-        # tokenized_text = self.remove_stopwords(tokenized_text)
+        tokenized_text = self.remove_stopwords(tokenized_text)
         return tokenized_text
-    
+
     def cosine_similarity(self, vec_n, vec_m):
         return 1 - spatial.distance.cosine(vec_n, vec_m)
 
@@ -58,23 +66,24 @@ class Preprocess():
         vec_n = []
         for word in bog_u:
             try:
-                vec_m.append(counts_m[word]/sum_m)
+                vec_m.append(counts_m[word] / sum_m)
             except:
                 vec_m.append(0)
             try:
-                vec_n.append(counts_n[word]/sum_n)
+                vec_n.append(counts_n[word] / sum_n)
             except:
                 vec_n.append(0)
+
         cosine = self.cosine_similarity(vec_n, vec_m)
         return cosine
-            
 
     def parse(self):
-        self.cleaned_questions = {key: {k: self.parse_string(v) if k in ['body', 'title'] else v for k, v in _value.items()} for key, _value in self.questions.items()}
-        
+        self.cleaned_questions = {
+            key: {k: self.parse_string(v) if k in ['body', 'title'] else v for k, v in _value.items()} for key, _value
+            in self.questions.items()}
+
 
 if __name__ == "__main__":
-    processing = Preprocess(output_path = 'processed/output')
+    processing = Preprocess(input_path='processed/output')
     # print(processing.parse_string("I'm new to C# and I want to use a track-bar to change a form's opacity. This is my code: decimal trans = trackBar Value / 5000; this.Opacity = trans; When I try to build it, I get this error: ' Cannot implicitly convert type 'decimal' to 'double' I tried making trans a double, but then the control doesn't work. This code worked fine for me in VB.NET. What do I need to do differently? you?"))
     # processing.parse()
-    
